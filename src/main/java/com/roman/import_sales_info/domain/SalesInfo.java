626fcffd762bde03219f893a48c81d5b7647da93
Roman Sherstiuk <ronmacdonald228@gmail.com>
2024-09-13 14:11:46 +0200

    SalesInfo anemic model

---
+++ src/main/java/com/roman/import_sales_info/domain/SalesInfo.java
@@ -0,0 +1,26 @@
+package com.roman.import_sales_info.domain;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class SalesInfo {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String product;
+    private String seller;
+    private Long sellerId;
+    private Double price;
+    private String city;
+    private String category;
+
+}
