af3ab88ee728353a97629c3bd8b03d980a144b62
Roman Sherstiuk <ronmacdonald228@gmail.com>
2024-09-13 13:12:35 +0200

    Initial Commit

---
+++ src/main/java/com/roman/import_sales_info/ImportSalesInfoApplication.java
@@ -0,0 +1,13 @@
+package com.roman.import_sales_info;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImportSalesInfoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImportSalesInfoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/roman/import_sales_info/ImportSalesInfoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.roman.import_sales_info;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImportSalesInfoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
