6e9ba6dba5f2957a20f1ba4c5550bcb3029a0447
Roman Sherstiuk <ronmacdonald228@gmail.com>
2024-09-13 14:49:50 +0200

    JpaItemWriter

--- src/main/java/com/roman/import_sales_info/batch/SalesInfoJobConfig.java
+++ src/main/java/com/roman/import_sales_info/batch/SalesInfoJobConfig.java
@@ -1,13 +1,17 @@
 package com.roman.import_sales_info.batch;
 
 import com.roman.import_sales_info.batch.dto.SalesInfoDto;
+import com.roman.import_sales_info.domain.SalesInfo;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.batch.item.database.JpaItemWriter;
+import org.springframework.batch.item.database.builder.JpaItemWriterBuilder;
 import org.springframework.batch.item.file.FlatFileItemReader;
 import org.springframework.batch.item.file.builder.FlatFileItemReaderBuilder;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.Resource;
 
 @Configuration
@@ -17,6 +21,7 @@ public class SalesInfoJobConfig {
 
     @Value("${input.csv.store}")
     private Resource resource;
+    private final EntityManagerFactory entityManagerFactory;
 
     public FlatFileItemReader<SalesInfoDto> salesInfoFileItemReader(){
         return new FlatFileItemReaderBuilder<SalesInfoDto>()
@@ -27,6 +32,12 @@ public class SalesInfoJobConfig {
                 .delimiter(",")
                 .names("product", "seller", "sellerId", "price", "city", "category")
                 .targetType(SalesInfoDto.class)
+                .build();
+    }
+
+    public JpaItemWriter<SalesInfo> salesInfoJpaItemWriter(){
+        return new JpaItemWriterBuilder<SalesInfo>()
+                .entityManagerFactory(entityManagerFactory)
                 .build();
     }
 }
