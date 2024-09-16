23a0dc2d776f6827f2aa1841d36a1cbd1f0ba723
Roman Sherstiuk <ronmacdonald228@gmail.com>
2024-09-16 10:38:50 +0200

    logger

--- src/main/java/com/roman/import_sales_info/batch/processor/SalesInfoItemProcessor.java
+++ src/main/java/com/roman/import_sales_info/batch/processor/SalesInfoItemProcessor.java
@@ -3,14 +3,17 @@ package com.roman.import_sales_info.batch.processor;
 import com.roman.import_sales_info.batch.dto.SalesInfoDto;
 import com.roman.import_sales_info.batch.mapper.SalesInfoMapper;
 import com.roman.import_sales_info.domain.SalesInfo;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.batch.item.ItemProcessor;
 import org.springframework.stereotype.Component;
 
 @Component
+@Slf4j
 public class SalesInfoItemProcessor implements ItemProcessor<SalesInfoDto, SalesInfo> {
     private SalesInfoMapper salesInfoMapper;
     @Override
     public SalesInfo process(SalesInfoDto item) throws Exception {
+        log.info("Processing the item: {}", item);
         return salesInfoMapper.INSTANCE.mapToEntity(item);
     }
 }
